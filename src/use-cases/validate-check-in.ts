d705ebdc972ce543d025247788d27130f6137421
Gustavoph <gusta.coliveira07@gmail.com>
2023-05-09 16:10:05 -0300

    feat: add time validate

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
+import dayjs from 'dayjs'
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found'
 import { CheckIn } from '@prisma/client'
+import { LateCheckInValidationError } from './errors/late-check-in-validation'
 
 namespace ValidateCheckIn {
   export type Params = {
@@ -22,6 +24,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
