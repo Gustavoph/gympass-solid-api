53910ec07f7363d35e60c29fdb08849ca18ed830
gustavoph <gusta.coliveira07@gmail.com>
2024-01-01 11:03:05 -0300

    chore: add eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev')
+  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
 })
 
 const _env = envSchema.safeParse(process.env)
